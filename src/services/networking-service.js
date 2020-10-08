34ef5441b5da830f6c3bd98fa71532101291bf4b
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-10-08 23:10:41 +0300

    (#3) Создал сервис для работы с сетью

--- src/services/networking-service.js
+++ src/services/networking-service.js
@@ -2,32 +2,18 @@ export default class NetworkingService {
 
   _apiBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
   _apiKey = '725cc83d358bd4a0ea4c6dcd990a60fb';
+  _proxy = 'https://cors-anywhere.herokuapp.com/';
 
 
-  async getResource(cityName) {
-    const res = await fetch(`${this._apiBase}${cityName}&appid${this._apiKey}`, {
-      mode: 'no-cors', 
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if(!res.ok) {
-      throw new Error('Could not fetch');
+  async getWeatherInfo(cityName) {
+    try {
+      const response = await fetch(`${this._proxy}${this._apiBase}${cityName}&appid=${this._apiKey}`);
+      return await response.json();
+    } catch(err) {
+      console.log(err);
     }
-    return await res.json();
-
   }
-
-  // getWeather() {
-  //   return this.getResource(cityName)
-  // }
-
 }
 
-const networking = new NetworkingService();
 
-networking.getResource('London').then((body) => {
-  console.log(body.name);
-})
+
