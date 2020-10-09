8be9b0af12867b6c9f1e4f927c6390338bc171ad
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 15:15:23 +0300

    (#15) Сделал компонент App statefull

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
