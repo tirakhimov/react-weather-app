381a91b222516640b20cd036687fd6dfb3674b05
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-10-09 16:45:02 +0300

    (#4) Сделал weather-app-service, который возвращает готовый объект погоды.

--- src/services/weather-app-service.js
+++ src/services/weather-app-service.js
@@ -1,10 +1,19 @@
 import NetworkingService from './networking-service';
 
-export default class WeatherAppService {
-
+export default class WeatherAppService extends NetworkingService {
+  
+  configureWeatherObject(cityName) {
+    super.getWeatherInfo(cityName).then((body) => {
+      const weatherObj = {
+        cityName: `${body.name}`,
+        temperature: `${Math.round(body.main.temp - 273)}`,
+        sky: `${body.weather[0].main}`,
+      }
+      console.log(weatherObj);
+      return weatherObj;
+    })
+  }
 }
 
-const networking = new NetworkingService();
-  networking.getWeatherInfo('London').then((body) => {
-    console.log(body);
-})
+const weatherAppService = new WeatherAppService();
+weatherAppService.configureWeatherObject('London');
