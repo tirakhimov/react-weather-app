17d2933bb491ac3f9e349ce81293215622dd15ae
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 19:07:49 +0300

    (#15) Исправил предупреждение про href

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,18 +5,18 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <a href="#" className="link navbar__logo-link">
+        <a href="/#" className="link navbar__logo-link">
           <h1>Weather App</h1>
         </a>
       </div>
       <div className="navbar__links">
-        <a href="#" className="link navbar__link">
+        <a href="/#" className="link navbar__link">
           Сегодня
         </a>
-        <a href="#" className="link navbar__link">
+        <a href="/#" className="link navbar__link">
           Завтра
         </a>
-        <a href="#" className="link navbar__link">
+        <a href="/#" className="link navbar__link">
           Три дня
         </a>
       </div>
