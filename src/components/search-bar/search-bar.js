8d7fae29abdb90f4f187bc1fb61bf62aa3d150be
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 15:23:53 +0300

    (#15) Добавил импорты для css

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './search-bar.css';
 
 function SearchBar() {
   return;
--- src/components/weather-card/weaher-card.js
+++ src/components/weather-card/weaher-card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './weather-card.css';
 
 function WeatherCard() {
   return;
