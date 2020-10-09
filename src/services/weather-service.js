4db182ade233bb606bd02805338ef1519d61b98d
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 20:14:18 +0300

    (#4) Добавил конструктор в weather-service и переименовал метод в getWeatherForToday

--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,19 +1,22 @@
 import NetworkingService from './networking-service';
 
-export default class WeatherAppService extends NetworkingService {
-  
-  configureWeatherObject(cityName) {
-    super.getWeatherInfo(cityName).then((body) => {
+export default class WeatherService {
+  #networkingService;
+
+  constructor(networkingService) {
+    this.#networkingService = networkingService;
+  }
+
+  getWeatherForToday(cityName) {
+    this.#networkingService.getWeatherInfo(cityName).then((body) => {
       const weatherObj = {
         cityName: `${body.name}`,
         temperature: `${Math.round(body.main.temp - 273)}`,
         sky: `${body.weather[0].main}`,
-      }
+      };
       console.log(weatherObj);
       return weatherObj;
-    })
+    });
   }
 }
 
-const weatherAppService = new WeatherAppService();
-weatherAppService.configureWeatherObject('London');
