80f23488191bbaec5f6afce0ca558b6dcea841f8
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 15:14:28 +0300

    (#15) Удалил сетевой код из компонента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import NetworkingService from '../../services/networking-service';
 import './app.css';
 
 
-function App() {
-  const networking = new NetworkingService();
-  networking.getWeatherInfo('London').then((body) => {
-    console.log(body);
-  })
 
   return (
     <div className="App">
