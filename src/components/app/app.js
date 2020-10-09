6d7c62073fc1b73f48940610879e1b4cc1343423
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 18:33:57 +0300

    (#15) Добавил верстку и стили для компонента App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from '../navbar/navbar';
+import WeatherCard from '../weather-card/weaher-card';
 import './app.css';
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="container">
+        <Navbar />
+        <WeatherCard />
+      </div>
+    );
   }
 }
 
