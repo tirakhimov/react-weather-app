3eec412fa10f6445a71da8e5d0301f084d00de94
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-09 18:34:36 +0300

    (#15) Добавил верстку и стили для компонента SearchBar

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './search-bar.css';
 
 function SearchBar() {
-  return;
+  return (
+    <div className="weather-card__content_input-wrapper">
+      <span className="weather-card__content_input-marker">&#128205;</span>
+      <input
+        className="input weather-card__content_input"
+        type="text"
+        placeholder="Поиск города или места"
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
