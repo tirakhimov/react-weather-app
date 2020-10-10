b6af4c10873d62ddaf3c62a99c8da78bb761107b
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-11 00:09:07 +0300

    (#4) Добавил отображение данных в компонент weather-card по статичному запросу

--- src/components/weather-card/weaher-card.js
+++ src/components/weather-card/weaher-card.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SearchBar from '../search-bar/search-bar';
 import './weather-card.css';
+import WeatherService from "../../services/weather-service";
 
-function WeatherCard() {
-  return (
-    <div>
-      <div className="weather-card">
-        <h3 className="weather-card__content_header">
-          Погода в Санкт-Петербурге
-        </h3>
-        <SearchBar />
-        <span className="weather-card__content_date"></span>
-        <div className="weather-card__content_display">
-          <div className="weather-card__content_emoji">
-            <p className="weather-card__content_emoji_p">&#127780;</p>
-          </div>
-          <div className="weather-card__content_temp">
-            <p className="weather-card__content_temp_p">14&deg;</p>
+export default class WeatherCard extends Component {
+
+  state ={}
+
+  weatherService = new WeatherService()
+
+  componentDidMount() {
+    this.updateState();
+  }
+
+  updateState() {
+    return this.weatherService.getWeatherForToday("london")
+      .then((response) => {
+        this.setState({
+          cityName: response.cityName,
+          sky: response.sky,
+          temperature: response.temperature,
+        })
+    });
+  }
+
+  render() {
+
+    const {cityName, sky, temperature} = this.state;
+
+    return (
+      <div>
+        <div className="weather-card">
+          <h3 className="weather-card__content_header">
+            Погода в {cityName}
+          </h3>
+          <SearchBar/>
+          <span className="weather-card__content_date"></span>
+          <div className="weather-card__content_display">
+            <div className="weather-card__content_emoji">
+              <p className="weather-card__content_emoji_p">{sky}</p>
+            </div>
+            <div className="weather-card__content_temp">
+              <p className="weather-card__content_temp_p">{temperature}&deg;</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
-
-export default WeatherCard;
