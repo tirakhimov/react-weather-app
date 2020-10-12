cbd27683a70098d87f822643018bbc6ad1e0d1a9
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-12 17:33:32 +0300

    Разрешил merge conflict в компоненте weather-card

--- src/components/weather-card/weaher-card.js
+++ src/components/weather-card/weaher-card.js
@@ -2,21 +2,51 @@ import React from 'react';
 import SearchBar from '../search-bar/search-bar';
 import './weather-card.css';
 
-function WeatherCard() {
-  return (
-    <div>
-      <div className="weather-card">
-        <h3 className="weather-card__content_header">
-          Погода в Санкт-Петербурге
-        </h3>
-        <SearchBar />
-        <span className="weather-card__content_date"></span>
-        <div className="weather-card__content_display">
-          <div className="weather-card__content_emoji">
-            <p className="weather-card__content_emoji_p">&#127780;</p>
-          </div>
-          <div className="weather-card__content_temp">
-            <p className="weather-card__content_temp_p">14&deg;</p>
+export default class WeatherCard extends Component {
+
+  state ={}
+
+  weatherService = new WeatherService()
+
+  componentDidMount() {
+    this.updateState();
+    if (this.state.weatherName) {
+      document.title = `Погода в ${this.state.weatherName}`;
+    }
+  }
+
+  updateState() {
+    return this.weatherService.getWeatherForToday("london")
+      .then((response) => {
+        this.setState({
+          cityName: response.cityName,
+          temperature: response.temperature,
+          weatherName: response.weatherName,
+        })
+    }).catch((err) => {
+        throw new Error(err.message);
+      });
+  }
+
+  render() {
+
+    const {cityName, weatherName, temperature} = this.state;
+
+    return (
+      <div>
+        <div className="weather-card">
+          <h3 className="weather-card__content_header">
+            Погода в {cityName}
+          </h3>
+          <SearchBar/>
+          <CurrentDate />
+          <div className="weather-card__content_display">
+            <div className="weather-card__content_emoji">
+              <p className="weather-card__content_emoji_p">{weatherName}</p>
+            </div>
+            <div className="weather-card__content_temp">
+              <p className="weather-card__content_temp_p">{temperature}&deg;</p>
+            </div>
           </div>
         </div>
       </div>
