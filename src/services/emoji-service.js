06337317d553ce82dd454e0d347be8711172858b
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-13 20:28:52 +0300

    (#31) Изменил EmojiService в соответсвии с eslint

--- src/services/emoji-service.js
+++ src/services/emoji-service.js
@@ -1,27 +1,24 @@
 export default class EmojiService {
-
-  _weatherEmojisObject = {
-    Thunderstorm: '⛈️',
-    Drizzle: '🌧️',
-    Rain: '🌧️',
-    Snow: '🌨️',
-    Mist: '🌫️',
-    Smoke: '🌫️',
-    Haze: '🌫️',
-    Dust: '🌫️',
-    Fog: '🌫️',
-    Ash: '🌫️',
-    Clear: '🔆',
-    Squall: '💨',
-    Tornado: '🌪',
-    Clouds: '☁️',
+  constructor() {
+    this.weatherEmojisObject = {
+      Thunderstorm: '⛈️',
+      Drizzle: '🌧️',
+      Rain: '🌧️',
+      Snow: '🌨️',
+      Mist: '🌫️',
+      Smoke: '🌫️',
+      Haze: '🌫️',
+      Dust: '🌫️',
+      Fog: '🌫️',
+      Ash: '🌫️',
+      Clear: '🔆',
+      Squall: '💨',
+      Tornado: '🌪',
+      Clouds: '☁️',
+    };
   }
 
   setEmojiFor(weatherName) {
-    for (let key in this._weatherEmojisObject) {
-      if(key === weatherName) {
-        return this._weatherEmojisObject[key];
-      }
-    }
+    return this.weatherEmojisObject[weatherName];
   }
 }
