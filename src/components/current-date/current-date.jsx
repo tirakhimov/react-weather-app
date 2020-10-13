833509e0303cc2a17bc596027f74a1c79c2bfd14
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-13 20:26:17 +0300

    (#31) Изменил расширение .jsx

--- src/components/current-date/current-date.jsx
+++ src/components/current-date/current-date.jsx
@@ -3,12 +3,11 @@ import './current-date.css';
 import DateFormatter from '../../services/date-formatter';
 
 export default function CurrentDate() {
-
   const dateFormatter = new DateFormatter();
 
   return (
-    <div className='weather-card__content_date'>
+    <div className="weather-card__content_date">
       <span>{dateFormatter.formatDate()}</span>
     </div>
-  )
+  );
 }
