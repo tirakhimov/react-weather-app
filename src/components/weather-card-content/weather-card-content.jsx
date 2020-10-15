56e1d1a6668fd272a16c86f3c1f386869226736c
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-15 21:14:56 +0300

    (#36) Исправил логику отрисовки компонента и сделал код более читаемым

--- src/components/weather-card-content/weather-card-content.jsx
+++ src/components/weather-card-content/weather-card-content.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
 import SearchBar from '../search-bar/search-bar';
 import CurrentDate from '../current-date/current-date';
-import ErrorComponent from '../error-component/error-component';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './weather-card-content.css';
 
 function WeatherCardContent({ weatherObject, onInputChange, hasError }) {
-  const { cityName, temperature, weatherName } = weatherObject;
+  const { cityName, ...tempAndWeatherName } = weatherObject;
+  const errorLabel = 'Такого города не существует';
 
   return (
     <>
       <h3 className="weather-card__content_header">
         Погода в <span className="capitalize">{cityName}</span>
       </h3>
-      <SearchBar
-        onInputSubmit={(inputValue) => onInputChange(inputValue)}
-      />
-      <CurrentDate />
-      {
-        hasError ? <ErrorComponent label="Такого города не сущетвует" /> : null
-      }
-      <div className="weather-card__content_display">
-        <span
-          role="img"
-          aria-label="Weather emoji"
-          className="weather-card__content_emoji"
-        >
-          { weatherName }
-        </span>
-        <div className="weather-card__content_temp">
-          <p className="weather-card__content_temp_p">
-            { temperature }
-            { temperature ? <span>&deg;</span> : null}
-          </p>
-        </div>
-      </div>
+      <SearchBar onInputSubmit={(inputValue) => onInputChange(inputValue)} />
+      { hasError ? <ErrorIndicator errorLabel={errorLabel} /> : <CurrentDate /> }
+      <EmojiAndTemperature tempAndWeatherName={tempAndWeatherName} />
     </>
   );
 }
