4ed41fc138da8cdbc1d905c30853d85b12687799
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-10-15 18:44:15 +0300

    (#36) Исправление ошибок Node CLI

--- src/components/weather-card-content/weather-card-content.jsx
+++ src/components/weather-card-content/weather-card-content.jsx
@@ -21,9 +21,23 @@ function WeatherCardContent({ weatherObject, onInputChange, hasError }) {
       }
       <div className="weather-card__content_display">
         {
-          hasError ? <span role="img" className="weather-card__content_emoji">&#128533;</span> : null
+          hasError ? (
+            <span
+              role="img"
+              aria-label="Sad emoji"
+              className="weather-card__content_emoji"
+            >
+              &#128533;
+            </span>
+          ) : null
         }
-        <span role="img" className="weather-card__content_emoji">{ weatherName }</span>
+        <span
+          role="img"
+          aria-label="Weather emoji"
+          className="weather-card__content_emoji"
+        >
+          { weatherName }
+        </span>
         <div className="weather-card__content_temp">
           <p className="weather-card__content_temp_p">
             { temperature }
