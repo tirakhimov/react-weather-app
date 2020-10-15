b0354e8bc965252d5ffcc43a701c841c24205e7d
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-15 21:14:21 +0300

    (#36) Вынес логику отрисовки емодзи и температуры в отдельный приватный компонент

--- src/components/weather-card-content/weather-card-content.jsx
+++ src/components/weather-card-content/weather-card-content.jsx
@@ -39,4 +39,22 @@ function WeatherCardContent({ weatherObject, onInputChange, hasError }) {
   );
 }
 
+const EmojiAndTemperature = ({ weatherName, temperature }) => (
+  <div className="weather-card__content_display">
+    <span
+      role="img"
+      aria-label="Weather emoji"
+      className="weather-card__content_emoji"
+    >
+      { weatherName }
+    </span>
+    <div className="weather-card__content_temp">
+      <p className="weather-card__content_temp_p">
+        { temperature }
+        { temperature ? <span>&deg;</span> : null}
+      </p>
+    </div>
+  </div>
+);
+
 export default WeatherCardContent;
