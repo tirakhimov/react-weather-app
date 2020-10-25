41cd74371481f4c176be8afa23ec96be918d4b4c
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-25 20:07:20 +0300

    (#50) Изменил верстку EmojiAndTemperature, используя Ant Design

--- src/components/emoji-and-temperature/emoji-and-temperature.jsx
+++ src/components/emoji-and-temperature/emoji-and-temperature.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Typography } from "antd";
 import './emoji-and-temperature.css';
 
 export default function EmojiAndTemperature({ tempAndWeatherName }) {
+  const { Text } = Typography;
+
   const {weatherName, temperature} = tempAndWeatherName;
 
   return (
     <div className="weather-card__content_display">
-      <span
+      <Text
         role="img"
         aria-label="Weather emoji"
         className="weather-card__content_emoji"
       >
         {weatherName}
-      </span>
+      </Text>
       <div className="weather-card__content_temp">
-        <p className="weather-card__content_temp_p">
+        <Text className="weather-card__content_temp_p">
           {temperature}
-          {temperature ? <span>&deg;</span> : null}
-        </p>
+          {temperature ? <Text>&deg;</Text> : null}
+        </Text>
       </div>
     </div>
   );
