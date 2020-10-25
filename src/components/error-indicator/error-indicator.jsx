a4e18fad567f29b26f2868608fca4a5070775e32
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-25 20:05:19 +0300

    (#50) Изменил верстку ErrorIndicator, используя Ant Design

--- src/components/error-indicator/error-indicator.jsx
+++ src/components/error-indicator/error-indicator.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Typography } from 'antd';
 
 import './error-indicator.css';
 
-function ErrorIndicator({errorLabel}) {
+function ErrorIndicator({ errorLabel }) {
+  const { Text } = Typography;
+
   return (
-    <div>
-      <div className="weather-card__content_message">{errorLabel}</div>
-      <span
+    <div className="weather-card__content_">
+      <Text
+        className="weather-card__content_message"
+        type="danger"
+      >
+        {errorLabel}
+      </Text>
+      <Text
         role="img"
         aria-label="Sad emoji"
         className="weather-card__content_emoji"
       >
         &#128533;
-      </span>
+      </Text>
     </div>
   );
 }
