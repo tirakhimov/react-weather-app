1a89ce09f1ae197d19f8c585d20c0e496d545e47
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-28 23:14:24 +0300

    (#46) Переписал WeatherCardHeader на TypeScript

--- src/components/weather-card-header/weather-card-header.tsx
+++ src/components/weather-card-header/weather-card-header.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './weather-card-header.css';
 import { Typography } from 'antd';
 
-export default function WeatherCardHeader({ cityName, hasError }) {
+export interface WeatherCardHeader {
+  cityName?: string;
+  hasError: boolean;
+}
+
+const WeatherCardHeader: React.FC<WeatherCardHeader> = ({ cityName, hasError }) => {
+
   const { Text } = Typography;
 
   const defaultHeaderText = 'Введите город';
   const headerTextAfterError = 'Упс... Что-то пошло не так';
   const headerTextAfterRequest = `Погода в ${cityName}`;
 
-  function headerSwitcher() {
+  function headerSwitcher(): string {
     if (hasError) {
       return headerTextAfterError;
     }
@@ -23,3 +29,5 @@ export default function WeatherCardHeader({ cityName, hasError }) {
     <Text strong className="weather-card__content_header">{headerSwitcher()}</Text>
   );
 }
+
+export default WeatherCardHeader;
