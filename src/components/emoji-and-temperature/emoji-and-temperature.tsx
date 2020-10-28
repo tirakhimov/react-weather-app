b428dc49933dff0e572d8e5cdb90f8cb303bbf1b
Timur Rakhimov <stayinlight97@gmail.com>
2020-10-28 23:13:13 +0300

    (#46) Переписал EmojiAndTemperature на TypeScript

--- src/components/emoji-and-temperature/emoji-and-temperature.tsx
+++ src/components/emoji-and-temperature/emoji-and-temperature.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { Typography } from "antd";
 import './emoji-and-temperature.css';
 
-export default function EmojiAndTemperature({ tempAndWeatherName }) {
+export interface EmojiAndTemperatureProps {
+  tempAndWeatherName: TempAndWeatherNameType;
+}
+
+export type TempAndWeatherNameType = {
+  weatherName?: string;
+  temperature?: string;
+}
+
+const EmojiAndTemperature: React.FC<EmojiAndTemperatureProps> = ({
+  tempAndWeatherName
+}): JSX.Element => {
+
   const { Text } = Typography;
 
   const {weatherName, temperature} = tempAndWeatherName;
 
   return (
     <div className="weather-card__content_display">
-      <Text
+      <span
         role="img"
         aria-label="Weather emoji"
         className="weather-card__content_emoji"
       >
         {weatherName}
-      </Text>
+      </span>
       <div className="weather-card__content_temp">
         <Text className="weather-card__content_temp_p">
           {temperature}
@@ -25,3 +37,5 @@ export default function EmojiAndTemperature({ tempAndWeatherName }) {
     </div>
   );
 }
+
+export default EmojiAndTemperature;
