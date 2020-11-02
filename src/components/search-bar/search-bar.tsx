c9525442acef40a81f6b597856ba91fee468aa0d
Timur Rakhimov <stayinlight97@gmail.com>
2020-11-02 23:35:22 +0300

    (#63) Переписал компонент SearchBar на React Hooks

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,58 +1,41 @@
-import React, { Component, KeyboardEvent, ChangeEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { Input } from 'antd';
+
 import './search-bar.css';
 
 export interface SearchBarProps {
   onInputSubmit: (inputValue: string) => void;
 }
 
-export interface SearchBarState {
-  inputValue: string;
-}
+const SearchBar: React.FC<SearchBarProps> = (props) => {
 
-export default class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  const [inputValue, setInputValue] = useState<string>('');
 
-  readonly state: SearchBarState;
-  private readonly handleSubmit: (event: KeyboardEvent<HTMLInputElement>) => void;
-  private readonly handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
-
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.state = { inputValue: '' };
-
-    this.handleSubmit = (event): void => {
-      if (event.key === 'Enter') {
-        const { onInputSubmit } = this.props;
-        const inputValue: string = event.currentTarget.value.replace(/ +/g, ' ').trim();
-
-        onInputSubmit(inputValue);
-        this.setState({
-          inputValue: '',
-        });
-      }
-    };
-
-    this.handleChange = (event): void => {
-      this.setState({
-        inputValue: event.currentTarget.value,
-      });
-    };
+  const handleSubmit = (event: KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      const {onInputSubmit} = props;
+      const inputValue: string = event.currentTarget.value
+        .replace(/ +/g, ' ').trim();
+      onInputSubmit(inputValue);
+      setInputValue(inputValue);
+    }
+  }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(event.currentTarget.value);
   }
 
-  render(): JSX.Element {
-    const { inputValue } = this.state;
-
-    return (
-      <div className="weather-card__content_input-wrapper">
-        <Input
-          className="input weather-card__content_input"
-          type="text"
-          placeholder="Поиск города или места"
-          onKeyDown={this.handleSubmit}
-          onChange={this.handleChange}
-          value={inputValue}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="weather-card__content_input-wrapper">
+      <Input
+        className="input weather-card__content_input"
+        type="text"
+        placeholder="Поиск города или места"
+        onKeyDown={handleSubmit}
+        onChange={handleChange}
+        value={inputValue}
+      />
+    </div>
+  );
 }
+
+export default SearchBar;
