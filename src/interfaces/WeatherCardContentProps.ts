fb9b4fd9df1b914c6ce99935049a835e24445276
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-03 14:51:09 +0300

    (#48) Вынес интерфейс WeatherCardContentProps в отдельный файл

---
+++ src/interfaces/WeatherCardContentProps.ts
@@ -0,0 +1,8 @@
+import {WeatherObject} from "./WeatherObject";
+
+export interface WeatherCardContentProps {
+  weatherObject: WeatherObject;
+  onInputChange: (inputValue: string) => void;
+  error?: Error;
+  currentDate: string;
+}
