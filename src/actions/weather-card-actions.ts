eb451ade999f680512f460a131bcaa2f090f715f
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-05 18:18:01 +0300

    (#48) Добавил логику для actions

--- src/actions/weather-card-actions.ts
+++ src/actions/weather-card-actions.ts
@@ -1,17 +1,35 @@
-import {Dispatch} from "redux";
+import {Dispatch} from 'redux';
+import {WeatherObject} from "../interfaces/WeatherObject";
+import WeatherService from "../services/weather-service";
 
 export const SEARCH_CITY = 'SEARCH_CITY';
-export const SET_DOCUMENT_TITLE = 'SET_DOCUMENT_TITLE';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const ERROR = 'ERROR';
 
-export const searchCity = (inputValue: string) => (dispatch: Dispatch): void => {
+// export const searchCity = {
+//   type: SEARCH_CITY,
+//   payload: inputValue,
+// };
+
+export const searchCity = (inputValue: string | undefined) => (dispatch: Dispatch) => {
   dispatch({
     type: SEARCH_CITY,
     payload: inputValue,
   });
-};
+}
 
-export const setDocumentTitle = () => (dispatch: Dispatch): void => {
-  dispatch({
-    type: SET_DOCUMENT_TITLE,
-  })
+export const fetchWeather = (inputValue: string | undefined) => (dispatch: Dispatch) => {
+  const weatherService = new WeatherService();
+  return weatherService.getWeatherForToday(inputValue)
+    .then((response: WeatherObject) => {
+      dispatch({
+        type: FETCH_WEATHER,
+        payload: response,
+      })
+    }).catch((error: Error) => {
+      dispatch({
+        type: ERROR,
+        payload: error,
+      })
+    });
 }
