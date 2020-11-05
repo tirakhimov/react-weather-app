02e99051eb36c4e05a5b14024b22d9902e149f03
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-05 18:15:05 +0300

    (#48) Удалил лишний файл

--- src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import weatherCardReducer from "./weather-card";
-
-const rootReducer = combineReducers({
-  weatherCard: weatherCardReducer,
-});
-
-export default rootReducer;
