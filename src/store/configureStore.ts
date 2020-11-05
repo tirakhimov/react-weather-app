c397d878859820cf4ba7c6d17c47d8edc133a38d
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-05 18:13:39 +0300

    (#48) Изменил логику конфигурирования Store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,7 +1,7 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
+import weatherCardReducer from "../reducers/weather-card";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(weatherCardReducer, applyMiddleware(thunk));
 
 export default store;
