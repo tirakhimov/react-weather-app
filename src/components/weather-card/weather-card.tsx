fed8ed2400a9184b93698a0a2480b560cf8da2ad
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-05 18:17:36 +0300

    (#48) Убрал лишнюю логику в компоненте

--- src/components/weather-card/weather-card.tsx
+++ src/components/weather-card/weather-card.tsx
@@ -1,65 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import { Card, Col, Row } from 'antd';
+import { connect } from 'react-redux';
 import WeatherCardContent from '../weather-card-content/weather-card-content';
-import { WeatherObject } from "../../interfaces/WeatherObject";
-import WeatherService from "../../services/weather-service";
-import DateFormatter from "../../services/date-formatter";
+import { WeatherCardProps } from '../../interfaces/WeatherCardProps';
 
 import './weather-card.css';
 
-const dateFormatter = new DateFormatter();
-const weatherService = new WeatherService();
-
-const WeatherCard: React.FC = () => {
-
-  const [ weatherObject, setWeatherObject ] = useState<WeatherObject>({});
-  const [ error, setError ] = useState<Error | undefined>(undefined);
-  const [ requestTime, setRequestTime ] = useState<number>(new Date().getTime());
-  const [ currentDate ] = useState<string>(dateFormatter.formatDate());
+const WeatherCard: React.FC<WeatherCardProps> = (props) => {
 
   useEffect(() => {
     const setDocumentTitle = (): void => {
-      document.title = `Погода в ${weatherObject.cityName}`;
+      document.title = `Погода в ${props.weatherObject.cityName}`;
     }
+    setDocumentTitle();
+  });
 
-    const updateState = (): Promise<void> | undefined => {
-      if (weatherObject.cityName !== undefined) {
-        return weatherService.getWeatherForToday(weatherObject.cityName)
-          .then((response) => {
-            setWeatherObject(() => {
-              return {
-                cityName: response.cityName,
-                temperature: response.temperature,
-                weatherName: response.weatherName,
-              }
-            })
-            setError(undefined);
-          }).catch((error) => {
-            setWeatherObject(() => {
-              return {}
-            });
-            setError(error);
-          });
-      }
-    }
-    updateState()?.then(() => setDocumentTitle());
-  }, [weatherObject.cityName, requestTime]);
-
-  const onInputChange = (inputValue: string): void => {
-    setWeatherObject( () => {
-        return { cityName: inputValue};
-      }
-    );
-    setRequestTime(new Date().getTime());
-  };
-
+  const { weatherObject, error, currentDate } = props;
   return (
     <Row justify="center" align="middle">
       <Col span={8}>
         <Card className="weather-card">
           <WeatherCardContent
             weatherObject={weatherObject}
-            onInputChange={onInputChange}
             error={error}
             currentDate={currentDate}
           />
@@ -69,4 +31,15 @@ const WeatherCard: React.FC = () => {
   );
 }
 
-export default WeatherCard;
+const mapStateToProps = (state: WeatherCardProps): WeatherCardProps => ({
+  weatherObject: state.weatherObject,
+  inputValue: state.inputValue,
+  error: state.error,
+  requestTime: state.requestTime,
+  currentDate: state.currentDate,
+});
+
+export default connect(
+  mapStateToProps,
+)(WeatherCard);
+
