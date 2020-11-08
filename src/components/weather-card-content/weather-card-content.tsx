862c0ca5092fd135df0c47599295766c73f32951
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-08 19:48:00 +0300

    (#65) Вернул объявление интерфейса в компонент
    т.к. используется единожды

--- src/components/weather-card-content/weather-card-content.tsx
+++ src/components/weather-card-content/weather-card-content.tsx
@@ -4,9 +4,15 @@ import CurrentDate from '../current-date/current-date';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import WeatherCardHeader from '../weather-card-header/weather-card-header';
 import EmojiAndTemperature from '../emoji-and-temperature/emoji-and-temperature';
-import {WeatherCardContentProps} from "../../interfaces/WeatherCardContentProps";
 
 import './weather-card-content.css';
+import {WeatherObject} from "../../interfaces/WeatherObject";
+
+interface WeatherCardContentProps {
+  weatherObject: WeatherObject;
+  error?: Error;
+  currentDate: string;
+}
 
 const WeatherCardContent: React.FC<WeatherCardContentProps> = ({
   weatherObject,
