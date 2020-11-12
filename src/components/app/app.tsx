c23445ec8500886f0d35ce8f1f05ba28511f6c33
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-12 16:09:17 +0300

    (#71) Добавил роутинг в корневом компоненте app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,24 +2,29 @@ import React from 'react';
 import { Col, Layout, Row } from 'antd';
 import Navbar from '../navbar/navbar';
 import WeatherCard from '../weather-card/weather-card';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './app.css';
+import WeatherCardTomorrow from '../weather-card-tomorrow/weather-card-tomorrow';
 
 const App: React.FC = (): JSX.Element => {
 
   const { Footer, Content } = Layout;
 
   return (
-    <Layout>
-      <Navbar />
-      <Content className="content">
-        <Row justify="center" align="middle">
-          <Col span={24}>
-            <WeatherCard />
-          </Col>
-        </Row>
-      </Content>
-      <Footer className="footer">Weather App ©2020 Created by ETD</Footer>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Navbar />
+        <Content className="content">
+          <Row justify="center" align="middle">
+            <Col span={24}>
+              <Route exact path="/" component={WeatherCard} />
+              <Route exact path="/tomorrow" component={WeatherCardTomorrow} />
+            </Col>
+          </Row>
+        </Content>
+        <Footer className="footer">Weather App ©2020 Created by ETD</Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
